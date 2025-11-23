4e68c53096c480388c7760d9c60d878500a0abaa
creamymood <wbwbwlgid@naver.com>
2025-11-23 12:17:56 +0900

    feat: 구현 조건에 맞게 라우터 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import About from "./pages/About";
+import MovieDetail from "./components/MovieDetail";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<About />} />
+        <Route path="/details" element={<MovieDetail />} />
       </Routes>
     </>
   );
