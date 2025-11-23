92c0be785dfacd2a87ea277f5e4bb74a9a911af8
creamymood <wbwbwlgid@naver.com>
2025-11-23 13:45:42 +0900

    feat: 메인페이지, 네브바 레이아웃 css 간단 구현

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 
 const NavBar = () => {
   return (
     <>
-      <p>로고</p>
-      <input type="text" />
-      <button>로그인</button>
-      <button>회원가입</button>
-
+      <Container>
+        <p>로고</p>
+        <input type="text" />
+        <MiniContainer>
+          <button>로그인</button>
+          <button>회원가입</button>
+        </MiniContainer>
+      </Container>
       <Outlet />
     </>
   );
 };
 
 export default NavBar;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  height: 10rem;
+`;
+
+const MiniContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
+
+const Button = styled.button``;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,23 @@ import React, { useState } from "react";
 import MovieCard from "../components/MovieCard";
 import movieListData from "../assets/movieListData.json";
 import MovieDetail from "../components/MovieDetail";
+import styled from "styled-components";
 
 const Home = () => {
   const [movie, setMovie] = useState(movieListData.results);
   return (
-    <div>
-      <p>메인으로 보여질 페이지 입니다</p>
+    <Card>
       {movie.map((movie) => {
         return <MovieCard key={movie.id} movie={movie} />;
       })}
-    </div>
+    </Card>
   );
 };
 
 export default Home;
+
+//style components
+const Card = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`;
