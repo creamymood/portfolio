9632d549baf827b3633cbef2d25fb7c8dbdaf48b
creamymood <wbwbwlgid@naver.com>
2025-11-23 16:44:05 +0900

    feat: 사전에 정의하지 않은 경로 진입시 404페이지 설정

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import MovieDetail from "./components/MovieDetail";
 import NavBar from "./components/NavBar";
+import { NotFound } from "./components/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<NavBar />}>
           <Route index element={<Home />} />
           <Route path="/details" element={<MovieDetail />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </>
